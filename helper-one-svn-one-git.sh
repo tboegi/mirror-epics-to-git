1d3f9cb4211de58cd69e3904aba04fcf751522ee
Torsten Bögershausen <tboegi@web.de>
2015-03-19 13:12:16 +0100

    helper-one-svn-one-git.sh: Don't translate branches into tags
    An svn branch is not a tag, so don't do the conversion

--- helper-one-svn-one-git.sh
+++ helper-one-svn-one-git.sh
@@ -117,21 +117,6 @@ addpacketifneeded svnadmin subversion &&
           cmd=$(echo git checkout remotes/origin/trunk)
           echo LINENO=$LINENO PWD=$PWD cmd=$cmd
           eval "$cmd" || exit 1
-        )  &&
-        (
-          pfx=origin/tags/
-          cd $subdir &&
-          tags=$(git branch -r | grep $pfx) &&
-          for rtag in $tags; do
-            ltag=$(echo $rtag | sed -e "s%$pfx%%g") &&
-            cmd=$(echo git checkout $rtag) &&
-            echo LINENO=$LINENO PWD=$PWD cmd=$cmd &&
-            eval "$cmd" || exit 1
-            cmd=$(echo git tag -f $ltag) &&
-            echo LINENO=$LINENO PWD=$PWD cmd=$cmd &&
-            eval "$cmd" || exit 1
-          done
-          git checkout origin/trunk
         )
       fi
     done
